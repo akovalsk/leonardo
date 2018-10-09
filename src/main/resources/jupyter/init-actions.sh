bdf5a7d2bcc1851f24a7ca3941b836595dcc6554
Azmina Karukappadath <akarukap@broadinstitute.org>
2018-10-09 13:33:00 -0400

    trying -y flag

--- src/main/resources/jupyter/init-actions.sh
+++ src/main/resources/jupyter/init-actions.sh
@@ -280,12 +280,14 @@ if [[ "${ROLE}" == 'Master' ]]; then
     log 'All done!'
 fi
 
-wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
-tar xvf Python-3.6.4.tgz
-cd Python-3.6.4
-./configure --enable-optimizations
-make -j8
-sudo make altinstall
+apt-get upgrade -y python3.6
+
+#wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
+#tar xvf Python-3.6.4.tgz
+#cd Python-3.6.4
+#./configure --enable-optimizations
+#make -j8
+#sudo make altinstall
 
 
 
